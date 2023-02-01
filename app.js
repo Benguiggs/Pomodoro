735217cb5ca7c0f5e9100c936b63b727a75b78ae
John Benguigui <benguiggs@outlook.fr>
2023-02-01 09:44:32 +0100

    add functions for reset time and animation in CSS

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 let initialTime = 1800;
-let  restTime = 300;
+let restTime = 300;
 
 function returnFormattedTime(time) {
-    return `${Math.trunc(time/60)}:${time % 60 < 10 ? `0${time % 60}` : time % 60}`    
+    return `${Math.trunc(time / 60)}:${time % 60 < 10 ? `0${time % 60}` : time % 60}`
 }
 
 const displayWork = document.querySelector(".work-display-time")
@@ -14,19 +14,23 @@ displayPause.textContent = returnFormattedTime(restTime)
 const startPauseBtn = document.querySelector(".start-btn")
 startPauseBtn.addEventListener("click", togglePomodoro)
 
+let currentInterval = false;
 let timerID;
-function togglePomodoro(){
+function togglePomodoro() {
     handlePlayPause()
 
-    initialTime --;
+    if (currentInterval) return;
+    currentInterval = true;
+
+    initialTime--;
     displayWork.textContent = returnFormattedTime(initialTime)
 
     timerID = setInterval(handleTicks, 1000)
 }
 
 let pause = true;
-function handlePlayPause(){
-    if(startPauseBtn.firstElementChild.src.includes("play")) {
+function handlePlayPause() {
+    if (startPauseBtn.firstElementChild.src.includes("play")) {
         startPauseBtn.firstElementChild.src = "ressources/pause.svg"
         pause = false;
     }
@@ -36,17 +40,30 @@ function handlePlayPause(){
     }
 }
 
+function handleClassAnimation(itemState) {
+    for (const item in itemState) {
+        if (itemState[item]) {
+            document.querySelector(` .${item}`).classList.add("active")
+        }
+        else {
+            document.querySelector(` .${item}`).classList.remove("active")
+        }
+    }
+}
+
 const cycles = document.querySelector(".cycles")
 let cyclesNumber = 0;
 
 function handleTicks() {
-    if (!pause && initialTime > 0){
+    if (!pause && initialTime > 0) {
         initialTime--;
         displayWork.textContent = returnFormattedTime(initialTime)
+        handleClassAnimation({ work: true, rest: false })
     }
     else if (!pause && initialTime === 0 && restTime > 0) {
         restTime--;
         displayPause.textContent = returnFormattedTime(restTime)
+        handleClassAnimation({ work: false, rest: true })
     }
     else {
         initialTime = 1799;
@@ -56,4 +73,22 @@ function handleTicks() {
         cyclesNumber++;
         cyclesNumber.textContent = `Cycle(s) : ${cyclesNumber}`
     }
+}
+
+const restBtn = document.querySelector(".reset-btn");
+restBtn.addEventListener("click", reset)
+
+function reset() {
+    initialTime = 1800;
+    restTime = 300;
+    displayWork.textContent = returnFormattedTime(initialTime)
+    displayPause.textContent = returnFormattedTime(restTime)
+
+    handleClassAnimation({ work: false, rest: false})
+
+    startPauseBtn.firstElementChild.src = "ressources/play.svg"
+    cycles.textContent = "cycle(s) : 0"
+
+    clearInterval(timerID);
+    currentInterval = false;
 }
